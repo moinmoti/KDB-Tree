0817c45c9f4e22e61ee6c0b49d8c1ba170a340df
Moin Hussain Moti <moinmoti@outlook.com>
2022-05-26 11:24:51 +0530

    Change default page and directory capacity.

--- run.sh
+++ run.sh
@@ -8,11 +8,11 @@ for q in ${Q[@]}; do
             QF="${q}/${t}-L1e7-I1e6-${w}H"
             echo "Experiments/${QF}"
             mkdir -p "Experiments/${QF}"
-            ./Index $PWD $QF 128 128
-            # lldb -- Index $PWD $QF 128 128
+            ./Index $PWD $QF 204 204
+            # lldb -- Index $PWD $QF 204 204
         done
     done
 done
 # mkdir -p "Experiments/Bulk/L7e7-Q8e3"
-# ./Index $PWD "Bulk/L7e7-Q8e3" 128 128
-# lldb -- Index $PWD "Bulk/L7e7-Q8e3" 128 128
+# ./Index $PWD "Bulk/L7e7-Q8e3" 204 204
+# lldb -- Index $PWD "Bulk/L7e7-Q8e3" 204 204
